bb2d64cffbcd1939eceb9baf63c03f45a5ab0212
Thomas Grady <tgrady@gatech.edu>
2021-09-11 17:46:51 -0400

    Refactor to simplify code and support nd FNO

---
+++ fno_nd.py
@@ -0,0 +1,60 @@
+import distdl
+import numpy as np
+import torch
+import torch.nn.functional as F
+import torch.nn as nn
+
+from collections import OrderedDict
+from distdl.utilities.torch import *
+from distdl.utilities.tensor_decomposition import *
+from spectral_conv import DistributedSpectralConvNd
+from torch import Tensor
+from typing import List, Optional
+
+Partition = distdl.backend.backend.Partition
+
+class DistributedFNONd(nn.Module):
+
+	def __init__(self,
+				 P_x: Partition,
+				 modes: List[int],
+				 width: int,
+				 num_blocks: int = 4) -> None:
+
+		super(DistributedFNONd, self).__init__()
+
+		self.P_x = P_x
+		self.P_y = P_x
+		self.modes = modes
+		self.width = width
+		self.num_blocks = num_blocks
+
+		self.dim = P_x.dim
+
+		self.si = ''.join(chr(i+97) for i in range(self.dim-2))
+		self.channel_mul_einsum_equation = f'xy{self.si},yz->xz{self.si}'
+		self.weight_mul_einsum_equation = f'xy{self.si},yz{self.si}->xz{self.si}'
+
+		self.fc0 = nn.Parameter(torch.rand(13, self.width))
+
+		self.convs = [DistributedSpectralConvNd(self.P_x, self.width, self.width, self.modes, P_y=self.P_x) for _ in range(self.num_blocks)]
+		self.weights = [nn.Parameter(torch.rand(self.width, self.width, *[1]*(self.dim-2))) for _ in range(self.num_blocks)]
+
+		self.fc1 = nn.Parameter(torch.rand(self.width, 128))
+		self.fc2 = nn.Parameter(torch.rand(128, 1))
+
+	def forward(self, x: Tensor) -> Tensor:
+
+		x = torch.einsum(self.channel_mul_einsum_equation, x, self.fc0)
+
+		for C, w in zip(self.convs, self.weights):
+			x1 = C(x)
+			x2 = torch.einsum(self.weight_mul_einsum_equation, x, w)
+			x = x1 + x2
+			x = F.gelu(x)
+
+		x = torch.einsum(self.channel_mul_einsum_equation, x, self.fc1)
+		x = F.gelu(x)
+		x = torch.einsum(self.channel_mul_einsum_equation, x, self.fc2)
+
+		return x
---
+++ test_fno_nd.py
@@ -0,0 +1,46 @@
+import distdl
+import numpy as np
+import torch
+
+from adjoint_test import check_adjoint_test_tight
+from collections import OrderedDict
+from distdl.utilities.slicing import compute_subshape
+from distdl.utilities.torch import *
+from mpi4py import MPI
+from torch import Tensor
+
+from fno_nd import DistributedFNONd
+
+Partition = distdl.backend.backend.Partition
+
+P_world = Partition(MPI.COMM_WORLD)
+P_0_base = P_world.create_partition_inclusive([0])
+P_0 = P_0_base.create_cartesian_topology_partition([1, 1, 1, 1, 1, 1])
+P_x_base = P_world.create_partition_inclusive(np.arange(16))
+P_x = P_x_base.create_cartesian_topology_partition([1, 1, 2, 2, 2, 2])
+
+x_global_shape = [8, 13, 16, 16, 16, 16]
+
+layer = DistributedFNONd(P_x, modes=[8, 8, 8, 8], width=20)
+
+x = zero_volume_tensor(x_global_shape[0])
+if P_x.active:
+	x_local_shape = compute_subshape(P_x.shape, P_x.index, x_global_shape)
+	x = torch.randn(*x_local_shape)
+
+x.requires_grad = True
+
+y = layer(x)
+dy = zero_volume_tensor(x_global_shape[0], dtype=y.dtype)
+if layer.P_y.active:
+	dy = torch.randn(*y.shape, dtype=y.dtype)
+
+y.backward(dy)
+dx = x.grad
+
+x = x.detach()
+dx = dx.detach()
+dy = dy.detach()
+y = y.detach()
+
+check_adjoint_test_tight(P_world, x, dx, y, dy)
