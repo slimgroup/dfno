a579f4329270104f85e78f70f24fcb45b4930c3b
Thomas Grady <tgrady@gatech.edu>
2021-09-22 01:59:16 -0400

    End to end experiment + fixes to dfno

--- fno_nd.py
+++ fno_nd.py
@@ -17,6 +17,8 @@ class DistributedFNONd(nn.Module):
 
     def __init__(self,
                  P_x: Partition,
+                 in_channels: int,
+                 out_timesteps: int,
                  modes: List[int],
                  width: int,
                  num_blocks: int = 4) -> None:
@@ -24,7 +26,14 @@ class DistributedFNONd(nn.Module):
         super(DistributedFNONd, self).__init__()
 
         self.P_x = P_x
+        self.P_t_shape = np.copy(self.P_x.shape)
+        self.P_t_shape[1] = self.P_x.shape[-1]
+        self.P_t_shape[-1] = self.P_x.shape[1]
+        self.P_t = self.P_x.create_cartesian_topology_partition(self.P_t_shape)
         self.P_y = P_x
+
+        self.in_channels = in_channels
+        self.out_timesteps = out_timesteps
         self.modes = modes
         self.width = width
         self.num_blocks = num_blocks
@@ -37,30 +46,45 @@ class DistributedFNONd(nn.Module):
         self.si = ''.join(chr(i+97) for i in range(self.dim-2))
         self.channel_mul_einsum_equation = f'xy{self.si},yz->xz{self.si}'
         self.weight_mul_einsum_equation = f'xy{self.si},yz{self.si}->xz{self.si}'
-
-        self.fc0 = nn.Parameter(torch.rand(13, self.width))
+        self.timestep_mul_einsum_equation = f'x{self.si}y,yz->x{self.si}z'
 
         self.convs = [DistributedSpectralConvNd(self.P_x, self.width, self.width, self.modes, P_y=self.P_x) for _ in range(self.num_blocks)]
+        self.norms = [distdl.nn.DistributedBatchNorm(self.P_x, num_features=self.width) for _ in range(self.num_blocks)]
         self.weights = [nn.Parameter(torch.rand(self.width, self.width, *[1]*(self.dim-2))) for _ in range(self.num_blocks)]
 
+        self.fc0 = nn.Parameter(torch.rand(self.in_channels, self.width))
         self.fc1 = nn.Parameter(torch.rand(self.width, 128))
         self.fc2 = nn.Parameter(torch.rand(128, 1))
+        self.fc3 = None
+
+        self.T0 = distdl.nn.DistributedTranspose(self.P_x, self.P_t)
+        self.T1 = distdl.nn.DistributedTranspose(self.P_t, self.P_x)
 
     def forward(self, x: Tensor) -> Tensor:
+
+        with torch.no_grad():
+            if self.fc3 is None:
+                self.fc3 = nn.Parameter(torch.rand(x.shape[-1], self.out_timesteps))
 
         if not self.P_x.active:
             return
 
         x = torch.einsum(self.channel_mul_einsum_equation, x, self.fc0)
 
-        for C, w in zip(self.convs, self.weights):
+        for C, N, w in zip(self.convs, self.norms, self.weights):
             x1 = C(x)
             x2 = torch.einsum(self.weight_mul_einsum_equation, x, w)
             x = x1 + x2
+            x = N(x)
             x = F.gelu(x)
 
         x = torch.einsum(self.channel_mul_einsum_equation, x, self.fc1)
         x = F.gelu(x)
         x = torch.einsum(self.channel_mul_einsum_equation, x, self.fc2)
+    
+        x = self.T0(x)
+        x = torch.einsum(self.timestep_mul_einsum_equation, x, self.fc3)
+        x = F.gelu(x)
+        x = self.T1(x)
 
         return x
