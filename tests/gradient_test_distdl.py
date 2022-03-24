49ce28ccddec90fc7aa73ae08ee028533d246c87
Thomas Grady <tgrady@gatech.edu>
2022-03-24 21:13:51 +0000

    Simplified distdl gradcheck ex

--- tests/gradient_test_distdl.py
+++ tests/gradient_test_distdl.py
@@ -7,8 +7,8 @@ from dfno.utils import create_standard_partitions
 from dfno import BroadcastedAffineOperator
 from gradient_test import gradient_test
 
-P_world, P_x, P_0 = create_standard_partitions((1, 1, 4))
-_, P_y, _ = create_standard_partitions((1, 4, 1))
+P_world, P_x, P_0 = create_standard_partitions((1, 2))
+_, P_y, _ = create_standard_partitions((2, 1))
 
 # This network passes gradcheck
 f = nn.Sequential(
@@ -18,15 +18,8 @@ f = nn.Sequential(
     nn.Linear(16, 16, dtype=torch.float64)
 )
 
-input_shape = (1, 4, 16)
-
-# Initialize lazy parameters
-with torch.no_grad():
-    x = torch.rand(*input_shape, dtype=torch.float64, device=torch.device('cpu'))
-    y = f(x)
-    del x
-    del y
-    gc.collect()
+# local shape
+input_shape = (2, 16)
 
 # Run test
 all_ok = True
@@ -51,19 +44,12 @@ P_x._comm.Barrier()
 f = nn.Sequential(
     nn.Linear(16, 16, dtype=torch.float64),
     dnn.DistributedTranspose(P_x, P_y),
-    nn.Linear(64, 64, dtype=torch.float64),
+    nn.Linear(32, 32, dtype=torch.float64),
     dnn.DistributedTranspose(P_y, P_x)
 )
 
-input_shape = (1, 4, 16)
-
-# Initialize lazy parameters
-with torch.no_grad():
-    x = torch.rand(*input_shape, dtype=torch.float64, device=torch.device('cpu'))
-    y = f(x)
-    del x
-    del y
-    gc.collect()
+# local shape
+input_shape = (2, 16)
 
 # Run test
 all_ok = True
@@ -73,8 +59,9 @@ for r in gradient_test(f, input_shape):
     if r.active:
         all_ok = all_ok and r.converged[0] and r.converged[1]
 
+P_x._comm.Barrier()
+
 if all_ok:
     print(f'rank {P_x.rank} passed gradcheck 2')
 else:
     print(f'rank {P_x.rank} failed gradcheck 2')
-
